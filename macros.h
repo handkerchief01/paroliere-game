8da879b3e4509e63d9f6145629c64cebe37e8f55
Mattia Sica <mattiasica10@gmail.com>
2025-03-03 10:45:09 +0000

    fix

--- macros.h
+++ macros.h
@@ -20,16 +20,21 @@
     }                                                                   \
   }
 
-#define SYSCN(v, c, m)    \
-  if ((v = c) == NULL)    \
-  {                       \
-    if (errno == EPIPE)   \
-    {                     \
-      pthread_exit(NULL); \
-    }                     \
-    else                  \
-    {                     \
-      perror(m);          \
-      pthread_exit(NULL); \
-    }                     \
+#define SYSCN(v, c, m)                                                  \
+  if ((v = c) == NULL)                                                  \
+  {                                                                     \
+    if (errno == EPIPE)                                                 \
+    {                                                                   \
+      pthread_exit(NULL);                                               \
+    }   \
+    else if (errno == EAGAIN || errno == EWOULDBLOCK)                   \
+    {                                                                   \
+      /* Timeout o socket non pronto => restituiamo -2 al chiamante. */ \
+      v = -2;                                                           \
+    }                                                                   \
+    else                                                                \
+    {                                                                   \
+      perror(m);                                                        \
+      pthread_exit(NULL);                                               \
+    }                                                                   \
   }
